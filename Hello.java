bbc111eaef42eee2cd99e2ed76c441e9b4db64ed
Mohini-Github <121348449+Mohini-Github@users.noreply.github.com>
2023-05-01 23:01:14 +0530

    Update Hello.java
    Additional changes

--- Hello.java
+++ Hello.java
@@ -1,6 +1,11 @@
 public class Hello {
     public static void main(String[] args)
     {
-        System.out.println("Hello students from Omar batch");
+        System.out.println("Hello students from Batch 28");
+        System.out.println("Mohini is best");
+        System.out.println("Mohini can do it alone");
+        System.out.println("God is with me");
+        System.out.println("Mohini is winner");
+        System.out.println("Today's is my day");
     }
 }
